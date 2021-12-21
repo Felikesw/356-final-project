596a87d0dc792825c60fed58bd90b721511ce060
Grace Lei <g2lei@uwaterloo.ca>
2021-12-21 15:02:08 -0500

    asdf

--- relational_schema/temp_tables/temp_population.sql
+++ relational_schema/temp_tables/temp_population.sql
@@ -16,7 +16,7 @@ CREATE TABLE temp_population (
     `Added on` VARCHAR(64)
 );
 
-CREATE TABLE 'start loading data' as '';
+SELECT 'start loading data' as '';
 
 LOAD DATA INFILE "/var/lib/mysql-files/06-COVID/target_tables/1_population/Management of patients who are underhoused or otherwise lower social economic status.csv"
 INTO TABLE temp_population
@@ -32,7 +32,7 @@ LINES TERMINATED BY "\n"
 IGNORE 1 LINES
 (Date, Study, `Study Link`, Journal, `Study Type`, `Addressed Population`, Challenge, Solution, `Strength of Evidence`, `Added on`);
 
-CREATE TABLE '1/3 done' as '';
+SELECT '1/3 done' as '';
 
 LOAD DATA INFILE "/var/lib/mysql-files/06-COVID/target_tables/1_population/Methods to control the spread in communities.csv"
 INTO TABLE temp_population
@@ -48,7 +48,7 @@ LINES TERMINATED BY "\n"
 IGNORE 1 LINES
 (Date, Study, `Study Link`, Journal, `Study Type`, `Addressed Population`, Challenge, Solution, `Strength of Evidence`, `Added on`);
 
-CREATE TABLE '2/3 done' as '';
+SELECT '2/3 done' as '';
 
 LOAD DATA INFILE "/var/lib/mysql-files/06-COVID/target_tables/1_population/What are recommendations for combating_overcoming resource failures_.csv"
 INTO TABLE temp_population
