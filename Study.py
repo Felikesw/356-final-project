9164f2356576b8f89d905ebe1a261d0fbf268555
ZIYI ZHUANG <z27zhuan@uwaterloo.ca>
2021-12-22 01:19:41 -0500

    fixed Study.py query generation code, added TUI

--- Study.py
+++ Study.py
@@ -1,13 +1,15 @@
 """
 TODO:
+    convert to date time for comparison
     double check the date time type
 """
 from enum import Enum
 
 
-class SearchType(Enum):
+class FilterType(Enum):
     SPECIFICATION = 1
     KEYWORDS = 2
+    ALL = 3
 
 
 class DateType(Enum):
@@ -19,20 +21,31 @@ class DateType(Enum):
 
 class Study:
     def __init__(self) -> None:
+        self.filter_type = FilterType.ALL  # how the query will filter the data
         self.study_type = None  # the study_type column in the table
         self.date_lower_bound = None  # the lower bound of the datetime
         self.date_upper_bound = None  # the upper bound of the datetime
-        self.keywords = []  # for searching keywords in both abstract and title
+        self.keywords = []  # for filtering keywords in both abstract and title
         self.table = "Study"  # the table that the query will be created based on
 
-    def reset():
+    def reset_study(self) -> None:
         """function that resets Study"""
+        self.filter_type = FilterType.ALL
         self.study_type = None
         self.date_lower_bound = None
         self.date_upper_bound = None
         self.keywords = []
 
-    def study_type_update(new_study_type, self) -> None:
+    def filter_type_update(self, new_filter_type) -> None:
+        """function that updates the filter_type
+
+        Args:
+            param1: new_filter_type : a FilterType for filter type
+
+        """
+        self.filter_type = new_filter_type
+
+    def study_type_update(self, new_study_type) -> None:
         """function that updates the study_type
 
         Args:
@@ -41,7 +54,7 @@ class Study:
         """
         self.study_type = new_study_type
 
-    def date_lower_update(new_date, self) -> None:
+    def date_lower_update(self, new_date) -> None:
         """function that updates the date_lower_bound
 
         Args:
@@ -50,7 +63,7 @@ class Study:
         """
         self.date_lower_bound = new_date
 
-    def date_upper_update(new_date, self) -> None:
+    def date_upper_update(self, new_date) -> None:
         """function that updates the date_upper_bound
 
         Args:
@@ -59,7 +72,7 @@ class Study:
         """
         self.date_upper_bound = new_date
 
-    def keywords_update(new_keywords, self) -> None:
+    def keywords_update(self, new_keywords) -> None:
         """function that updates the keywords
 
         Args:
@@ -67,63 +80,64 @@ class Study:
 
         """
         if new_keywords:
-            # split the string using ',', then strip the space around each element
-            self.keywords = [k.strip() for k in new_keywords.split(",")]
+            # split the string using ',', then strip the space around each element,
+            # then remove all the empty strings in the list
+            self.keywords = list(
+                filter(None, [k.strip() for k in new_keywords.split(",")])
+            )
         else:
             print(
-                "[ERROR] Invalid keywords format. Please seperate the keywords with a comma when entering multiple. e.x. Covid, reviews"
+                "[ERROR] Invalid keywords format. Please seperate the keywords with a comma when entering multiple (e.x. Covid, reviews)."
             )
 
-    def create_query(search_type, limit, self):
+    def create_query(self, limit) -> str:
         """function that creates and return a sql query
 
         Args:
-            param1: search_type : a SearchType for determining how the query will be created
-            param2: limit : an int for the record limit for the query
+            param1: limit : an int for the record limit for the query
 
         Returns:
-            returns null if any errors occurs, else return a stirng containing the query created
+            returns an empty string if any errors occurs, else return a stirng containing the query created
         """
         query = "SELECT * "
 
-        # query with specified requirements
-        if search_type == SearchType.SPECIFICATION:
+        # filter with specified attributes
+        if self.filter_type == FilterType.SPECIFICATION:
             date_type = DateType.NOTGIVEN
+            first_keyword = True
             # study_type clause
             if self.study_type != None:
-                qyery += f" WHERE study_type = '{self.study_type}' "
+                query += f"WHERE study_type = '{self.study_type}' "
+                first_keyword = False
             # date clause
             if self.date_lower_bound != None:
-                # error check
-                if self.date_lower_bound > self.date_upper_bound:
-                    print(
-                        "[ERROR] Date lower bound higher than date upper bound, please make sure the upper bound >= lower bound."
-                    )
-                    return None
                 date_type = DateType.ONLYLOWER
 
             if self.date_upper_bound != None:
-                if self.date_lower_bound > self.date_upper_bound:
-                    print(
-                        "[ERROR] Date lower bound higher than date upper bound, please make sure the upper bound >= lower bound."
-                    )
-                    return None
                 date_type = (
                     DateType.ONLYUPPER
                     if date_type == DateType.NOTGIVEN
                     else DateType.BOTH
                 )
             # when filtering with only lower bound
+            if not first_keyword and date_type != DateType.NOTGIVEN:
+                query += "AND"
             if date_type == DateType.ONLYLOWER:
-                qyery += f" WHERE date >= '{self.date_lower_bound}' "
+                query += f" WHERE date >= '{self.date_lower_bound}' "
             # when filtering with only an upper bound
             elif date_type == DateType.ONLYUPPER:
-                qyery += f" WHERE date <= '{self.date_upper_bound}' "
+                query += f" WHERE date <= '{self.date_upper_bound}' "
             # when filtering with a lower bound and an upper bound
             elif date_type == DateType.BOTH:
-                qyery += f" WHERE date between '{self.date_lower_bound}' and '{self.date_upper_bound}' "
-        # search by keywords
-        elif search_type == SearchType.KEYWORDS:
+                # error check
+                if self.date_lower_bound > self.date_upper_bound:
+                    print(
+                        "[ERROR] Date lower bound higher than date upper bound, please make sure the upper bound >= lower bound."
+                    )
+                    return ""
+                query += f" WHERE date BETWEEN '{self.date_lower_bound}' AND '{self.date_upper_bound}' "
+        # filter by keywords
+        elif self.filter_type == FilterType.KEYWORDS:
             if len(self.keywords) > 0:
                 first_keyword = True
                 for keyword in self.keywords:
@@ -134,5 +148,5 @@ class Study:
                         f" title LIKE '%{keyword}%' OR abstract LIKE '%{keyword}%' "
                     )
 
-        query += f" FROM {self.table} LIMIT {limit}"
+        query += f"FROM {self.table} LIMIT {limit};"
         return query
--- main.py
+++ main.py
@@ -1,26 +1,219 @@
 import mysql.connector
 import getpass
 import logging
+from Study import Study, FilterType
+import time
+
+DEFAULT_RECORD_LIMIT = 1000
+# limit of how many records to return
+RECORD_LIMIT = 1000
 
 
 def main():
     # ask for user name
     username = input("Enter Username: ")
-    # connect to the db
-    mydb = mysql.connector.connect(
-        host="marmoset03.shoshin.uwaterloo.ca",
-        user=username,
-        password=getpass.getpass(),
-        database=f"db356_{username}",
-    )
-    cursor = mydb.cursor()
+    # # connect to the db
+    # mydb = mysql.connector.connect(
+    #     host="marmoset03.shoshin.uwaterloo.ca",
+    #     user=username,
+    #     password=getpass.getpass(),
+    #     database=f"db356_{username}",
+    # )
+    # cursor = mydb.cursor()
 
-    # limit of how many records to return
-    record_limit = 1000
-
-    # show options for user to choose from: add new data, serach for studies, search for questions, search for risk factors
+    initial_selection()
     # make a class for questions
     # make a class for risk factors
+
+
+def initial_selection() -> None:
+    """initial selection for the user: search for studies, search for risk factors, search for questions, add new data"""
+    time.sleep(0.5)
+    option = int(
+        input(
+            """
+===========================================
+Please select one of the following options: 
+-------------------------------------------
+1. Search for studies 
+2. Search for risk factors
+3. Search for questions
+4. Input new data
+5. Exit
+-------------------------------------------
+"""
+        )
+    )
+
+    if option == 1:
+        study_selection()
+    elif option == 2:
+        print(
+            "\n[INFO] Some new and exciting features are being developed, come back later to check it out!"
+        )
+
+    elif option == 3:
+        print(
+            "\n[INFO] Some new and exciting features are being developed, come back later to check it out!"
+        )
+
+    elif option == 4:
+        print(
+            "\n[INFO] Some new and exciting features are being developed, come back later to check it out!"
+        )
+
+    elif option == 5:
+        print("\n[INFO] Program exited successfully.\n")
+        # terminate program
+        quit()
+    else:
+        print("\n[ERROR] Invalid selection, please try again.")
+
+    initial_selection()
+
+
+def study_selection():
+    """select filter type for Study table:"""
+    time.sleep(0.5)
+    option = int(
+        input(
+            f"""
+===========================================
+Please select one of the following options: 
+-------------------------------------------
+(current limit: {RECORD_LIMIT} records)
+1. Filter with specific attributes
+2. Filter with keywords
+3. Return all records up to record limit 
+4. Change record limit 
+5. Back
+-------------------------------------------
+"""
+        )
+    )
+
+    study = Study()
+    create_query = False
+    query = ""
+
+    if option == 1:
+        study_type = input(
+            f"""
+-------------------------------------------------------------------------------------------------------------------------
+Please enter the desired study_type value, or press enter to continue with the default value. (case and space sensitive): 
+-------------------------------------------------------------------------------------------------------------------------
+"""
+        )
+        if study_type != "":
+            study.study_type_update(study_type.strip())
+
+        date_lower_bound = input(
+            f"""
+-------------------------------------------------------------------------------------------------------------------------
+Please enter the desired date lower bound value, or press enter to continue with the default value. (format: yyyy-mm-dd): 
+-------------------------------------------------------------------------------------------------------------------------
+"""
+        )
+        if date_lower_bound != "":
+            study.date_lower_update(date_lower_bound.strip())
+
+        date_upper_bound = input(
+            f"""
+-------------------------------------------------------------------------------------------------------------------------
+Please enter the desired date upper bound value, or press enter to continue with the default value. (format: yyyy-mm-dd): 
+-------------------------------------------------------------------------------------------------------------------------
+"""
+        )
+        if date_upper_bound != "":
+            study.date_upper_update(date_upper_bound.strip())
+
+        study.filter_type_update(FilterType.SPECIFICATION)
+        create_query = True
+
+    elif option == 2:
+        keywords = input(
+            f"""
+------------------------------------------------------------------------------------------------------------------------
+Please enter the keywords for filtering the data, or press enter key to continue with the default value. 
+Please seperate the keywords with a comma when entering multiple (e.x. Covid, reviews, news) (case and space sensitive): 
+------------------------------------------------------------------------------------------------------------------------
+"""
+        )
+        if keywords != "":
+            study.keywords_update(keywords)
+
+        study.filter_type_update(FilterType.KEYWORDS)
+        create_query = True
+
+    elif option == 3:
+        create_query = True
+
+    elif option == 4:
+        limit_update()
+
+    elif option == 5:
+        initial_selection()
+
+    else:
+        print("\n[ERROR] Invalid selection, please try again.")
+
+    if create_query:
+        query = study.create_query(RECORD_LIMIT)
+        print(f"query: {query}")
+
+    study_selection()
+
+
+def limit_update():
+    """update the record limit"""
+    time.sleep(0.5)
+    option = int(
+        input(
+            f"""
+===========================================
+Please select one of the following options: 
+-------------------------------------------
+(current limit: {RECORD_LIMIT} records)
+1. New limit
+2. Restore default ({DEFAULT_RECORD_LIMIT} records)
+3. Back
+-------------------------------------------
+"""
+        )
+    )
+
+    if option == 1:
+        new_limit = int(
+            input(
+                f"""
+-------------------------------------------
+Please enter the new positive integer limit: 
+-------------------------------------------
+"""
+            )
+        )
+        # maybe error check
+        modify_RECORD_LIMIT(new_limit)
+    elif option == 2:
+        modify_RECORD_LIMIT(DEFAULT_RECORD_LIMIT)
+    elif option == 3:
+        return
+    else:
+        print("\n[ERROR] Invalid selection, please try again.")
+        limit_update()
+
+    print(f"\n[INFO] Updated limit: {RECORD_LIMIT} records.")
+
+
+def modify_RECORD_LIMIT(new_limit):
+    """function that modifies the global variable RECORD_LIMIT
+
+    Args:
+        param1: new_limit : an int for the new record limit
+
+    """
+    global RECORD_LIMIT
+    RECORD_LIMIT = new_limit
 
 
 if __name__ == "__main__":
